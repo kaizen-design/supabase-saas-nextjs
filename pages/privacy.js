cf60f945205564cfa4b6388e0a6ccf1a68752639
denis <denis@kaizen-design.ru>
2022-07-14 20:09:14 +0400

    Allow Customer to Manage Their Subscription with Stripe Customer Portal

---
+++ pages/privacy.js
@@ -0,0 +1,7 @@
+const Privacy = () => {
+  return (
+    <h1>Privacy policy</h1>
+  )
+}
+
+export default Privacy;
---
+++ pages/terms.js
@@ -0,0 +1,7 @@
+const Terms = () => {
+  return (
+    <h1>Terms</h1>
+  )
+}
+
+export default Terms;
